9ae93bf9cb44252698cd995ad5a58ca4f095d1cb
Dulina100 <dediyagala2003@gmail.com>
2026-05-15 22:34:23 +0530

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+console.log("js loaded!!")
+
+let browseBtn = document.getElementById("browse btn");
+       browseBtn.addEventListener("click",()=>{
+
+           window.location.href = "browse.html";
+
+       });
+
+let apikey="f56e9cff";
+
+let movie= document.getElementById("txt_movie");
+
+movie.addEventListener("keypress",e =>{
+         if(e.key=="Enter"){
+            apiCall(movie.value)
+         }
+})
+
+let apiCall =  async (movie) => {
+       await fetch(`http://www.omdbapi.com/?s=${movie}&apikey=${apikey}`)
+      .then(res=>res.json())
+      .then(data=>{
+           setMovie(data.Search[0]);
+      })
+} 
+
+let setMovie =(data)=>{
+       let poster=document.getElementById("poster");
+       let title=document.getElementById("Intesteller");
+       let year=document.getElementById("2014");
+       let genre=document.getElementById("Science");
+       let imdb=document.getElementById("imdb")
+       let director=document.getElementById("director");
+       let actors=document.getElementById("actors");
+       let plot=document.getElementById("plot");
+       let imdb_rating=document.getElementById("imdb_rating");
+
+       poster.src=data.Poster;
+       title.innerText= data.Title;
+       year.innerText= data.Year;
+       genre.innerText= data.Type;
+       imdb.innerText=data.imdbID;
+
+       
+
+    //    director.innerText= data.
+    //    actors.innerText= data.
+    //    plot.innerText= data.
+    //    imdb_rating.innerText= data.
+
+}
---
+++ browse.js
@@ -0,0 +1,50 @@
+let apikey = "f56e9cff";
+
+let movieContainer = document.getElementById("movie_container");
+
+let movies = [
+    "batman",
+    "avengers",
+    "joker",
+    "interstellar",
+    "superman",
+    "spiderman 2",
+    "ghost rider",
+    "transformers"
+];
+
+async function loadMovies() {
+
+    for(let movie of movies){
+
+        let response = await fetch(
+            `http://www.omdbapi.com/?t=${movie}&apikey=${apikey}`
+        );
+
+        let data = await response.json();
+
+        createMovieCard(data);
+
+    }
+
+}
+
+function createMovieCard(movie){
+
+    let div = document.createElement("div");
+
+    div.className = "movie_card";
+
+    div.innerHTML = `
+    
+        <img src="${movie.Poster}">
+        <h3>${movie.Title}</h3>
+        <p>${movie.Year}</p>
+
+    `;
+
+    movieContainer.appendChild(div);
+
+}
+
+loadMovies();
